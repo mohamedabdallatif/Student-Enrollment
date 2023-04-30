592072e95e3895f20142aa14ef60d952025730a0
mohamedabdallatif <muhammed195841@feng.bu.edu.eg>
2023-05-01 00:07:29 +0200

    edit

--- lib/connection.php
+++ lib/connection.php
@@ -1,8 +1,8 @@
 
 <?php
-$dns = ‘mysql:host=localhost;dbname=id20681932_students’;
-$user = ‘id20681932_studentssqlserver123’;
-$password = ‘atxjeQ)*klWvOd7kAdPO’;
+$dns = 'mysql:host=localhost;dbname=id20681932_students';
+$user = 'id20681932_studentssqlserver123';
+$password = 'atxjeQ)*klWvOd7kAdPO';
 try{
  $db = new PDO ($dns, $user, $pass);
 }catch( PDOException $e){
