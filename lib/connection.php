842d079e9b8fbae8a857971353639b71682952c2
mohamedabdallatif <muhammed195841@feng.bu.edu.eg>
2023-05-01 00:10:00 +0200

    pass edit

--- lib/connection.php
+++ lib/connection.php
@@ -2,7 +2,7 @@
 <?php
 $dns = 'mysql:host=localhost;dbname=id20681932_students';
 $user = 'id20681932_studentssqlserver123';
-$password = 'atxjeQ)*klWvOd7kAdPO';
+$pass = 'atxjeQ)*klWvOd7kAdPO';
 try{
  $db = new PDO ($dns, $user, $pass);
 }catch( PDOException $e){
