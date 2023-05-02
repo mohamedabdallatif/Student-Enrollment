3adfe1b7ba892b8ae73996e157925a9ff5a91d69
mohamedabdallatif <muhammed195841@feng.bu.edu.eg>
2023-05-02 10:43:45 +0200

    php script edit

--- lib/get.php
+++ lib/get.php
@@ -10,4 +10,36 @@ catch(PDOException $e)
 {
     echo $e->getMessage();
 }
+
+/*
+<?php
+// Replace these variables with your MySQL database credentials
+$host = 'localhost';
+$username = 'your_username';
+$password = 'your_password';
+$database = 'your_database';
+
+// Create a new MySQL connection
+$conn = mysqli_connect($host, $username, $password, $database);
+
+// Check for connection errors
+if (mysqli_connect_errno()) {
+  die('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+
+// Fetch data from MySQL database
+$sql = "SELECT * FROM your_table";
+$result = mysqli_query($conn, $sql);
+
+// Convert the MySQL result into a JSON-encoded string
+$rows = array();
+while ($row = mysqli_fetch_assoc($result)) {
+  $rows[] = $row;
+}
+echo json_encode($rows);
+
+// Close the MySQL connection
+mysqli_close($conn);
 ?>
+
+*/
