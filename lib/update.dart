88a09c298b67d07a6e5bf4ad6eb7b6e7c8740d89
mohamedabdallatif <muhammed195841@feng.bu.edu.eg>
2023-05-05 01:38:28 +0200

    update

--- lib/update.dart
+++ lib/update.dart
@@ -1,22 +1,205 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
 class UpdatePage extends StatefulWidget {
-  const UpdatePage({super.key});
+var element;
+   UpdatePage({
+    Key? key,
+    required this.element,
+  }) : super(key: key);
 
   @override
   State<UpdatePage> createState() => _UpdatePageState();
 }
 
 class _UpdatePageState extends State<UpdatePage> {
+
   @override
   Widget build(BuildContext context) {
+      var gender='${widget.element['Gender']}';
     return  Scaffold(
       appBar: AppBar(
         title: const Text('Update student'),
         centerTitle: true,
         backgroundColor: Colors.teal,
       ),
-      body: null,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Form(
+            child: Column(
+              children: [
+                const Text(
+                  'Student\'s Detials',
+                  style: TextStyle(fontSize: 20),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  initialValue: '${widget.element['First_Name']}',
+                //  controller: fnamecontroller,
+                  decoration: const InputDecoration(
+                      label: Text('First name'),
+                      prefixIcon: Icon(Icons.person),
+                      border: OutlineInputBorder()),
+                  validator: (value) {
+                    if (value!.length > 100) {
+                      return 'First name can\'t be greater than 100';
+                    }
+                    if (value.length < 2) {
+                      return 'First name can\'t be lesser than 2';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                   initialValue: '${widget.element['Last_Name']}',
+                //  controller: lnamecontroller,
+                  decoration: const InputDecoration(
+                      label: Text('Last name'),
+                      prefixIcon: Icon(Icons.person),
+                      border: OutlineInputBorder()),
+                  validator: (value) {
+                    if (value!.length > 100) {
+                      return 'Last name can\'t be greater than 100';
+                    }
+                    if (value.length < 2) {
+                      return 'Last name can\'t be lesser than 2';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                   initialValue: '${widget.element['Date_Of_Birth']}',
+                 // controller: dateController,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Date Of Birth can\'t br empty';
+                    }
+                    return null;
+                  },
+                  onTap: () {
+                    showDatePicker(
+                            context: context,
+                            initialDate: DateTime.now(),
+                            firstDate: DateTime.utc(1900),
+                            lastDate: DateTime.now())
+                        .then((value) {
+                   //   dateController.text = value!.toIso8601String();
+                    });
+                  },
+                  keyboardType: TextInputType.none,
+                  decoration: const InputDecoration(
+                      label: Text('Date of Birth'),
+                      prefixIcon: Icon(Icons.date_range),
+                      border: OutlineInputBorder()),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                   initialValue: '${widget.element['Address']}',
+                //  controller: addresscontroller,
+                  keyboardType: TextInputType.text,
+                  decoration: const InputDecoration(
+                      label: Text('Address'),
+                      prefixIcon: Icon(Icons.home),
+                      border: OutlineInputBorder()),
+                  validator: (value) {
+                    if (value!.length > 100) {
+                      return 'Address can\'t be greater than 100';
+                    }
+                    if (value.length < 2) {
+                      return 'Address can\'t be lesser than 2';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                   initialValue: '${widget.element['Religion']}',
+                 // controller: religioncontroller,
+                  keyboardType: TextInputType.text,
+                  decoration: const InputDecoration(
+                      label: Text('Religion'),
+                      prefixIcon: Icon(Icons.mosque),
+                      border: OutlineInputBorder()),
+                  validator: (value) {
+                    if (value!.length > 100) {
+                      return 'Religion can\'t be greater than 100';
+                    }
+                    if (value.length < 2) {
+                      return 'Religion can\'t be lesser than 2';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                   initialValue: '${widget.element['Nationality']}',                  
+              //    controller: nationalitycontroller,
+                  keyboardType: TextInputType.text,
+                  decoration: const InputDecoration(
+                      label: Text('Nationality'),
+                      prefixIcon: Icon(Icons.airlines),
+                      border: OutlineInputBorder()),
+                  validator: (value) {
+                    if (value!.length > 100) {
+                      return 'Nationality can\'t be greater than 100';
+                    }
+                    if (value.length < 2) {
+                      return 'Nationality can\'t be lesser than 2';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 10),
+                  alignment: Alignment.topLeft,
+                  child: const Text(
+                    'Gender',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+                RadioListTile(
+                    title: const Text('Male'),
+                    value: 'Male',
+                    groupValue: gender,
+                    contentPadding: EdgeInsets.zero,
+                    onChanged: (value) {
+                      setState(() {
+                        gender = value!;
+                      });
+                    }),
+                RadioListTile(
+                    title: const Text('Female'),
+                    value: 'Female',
+                    contentPadding: EdgeInsets.zero,
+                    groupValue: gender,
+                    onChanged: (value) {
+                      setState(() {
+                        gender = value!;
+                      });
+                    }),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
