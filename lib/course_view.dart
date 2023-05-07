0b21cfe07fbb3ad4c4d5570dea6a18ac6a153e68
MohamedMamdouh-432 <mohammedmamdouh432@gmail.com>
2023-05-07 03:06:36 +0300

    comm

--- lib/course_view.dart
+++ lib/course_view.dart
@@ -13,36 +13,45 @@ class CourseView extends StatefulWidget {
 }
 
 class _CourseViewState extends State<CourseView> {
-  List courses = ['Arabic', 'English', 'C', 'Java'];
+  List<String> courses = ['Arabic', 'English', 'C', 'Java'];
   List isClicked = [false, false, false, false];
   List<String> selectedCourses = [];
-  var insertUrl = 'https://studentssqlserver123.000webhostapp.com/insert.php';
-  // List<String> coursesList = widget.element['Course_Name'].split(",");
-  // Future insertStudent(String fname, String lname, String date, String Address,
-  //     String religion, String nationality, String gender, List list) async {
-  //   final response = await http.post(Uri.parse(insertUrl), body: {
-  //     'fname': fname,
-  //     'lname': lname,
-  //     'date': date,
-  //     'Address': Address,
-  //     'religion': religion,
-  //     'nationality': nationality,
-  //     'gender': gender,
-  //     'list': list.toString(),
-  //   });
-  //   return response;
-  // }
+
+  var updateUrl = 'https://studentssqlserver123.000webhostapp.com/update.php';
+
+  Future updateStudent(int id, String fname, String lname, String date, String address,
+      String religion, String nationality, String gender, List list) async {
+    final response = await http.post(Uri.parse(updateUrl), body: {
+      'id': id,
+      'fname': fname,
+      'lname': lname,
+      'date': date,
+      'Address': address,
+      'religion': religion,
+      'nationality': nationality,
+      'gender': gender,
+      'list': list.toString(),
+    });
+    return response;
+  }
 
   @override
   void initState() {
     selectedCourses = widget.curstd['Course_Name'].split(",");
-    print(selectedCourses);
-    // for (int i = 0; i < courses.length; i++) {
-    //   if (widget.courselist!.contains(courses[i])) {
-    //     isClicked[i] = true;
-    //     selectedCourses.add(courses[i]);
-    //   }
-    // }
+    String f = selectedCourses.first;
+    f = f.substring(1);
+    selectedCourses.first = f;
+    f = selectedCourses.last;
+    f = f.substring(0, f.length - 1);
+    selectedCourses.last = f;
+    for (int i = 0; i < selectedCourses.length; i++) {
+      selectedCourses[i] = selectedCourses[i].trim();
+    }
+    for (int i = 0; i < courses.length; i++) {
+      if (selectedCourses.contains(courses[i])) {
+        isClicked[i] = true;
+      }
+    }
     super.initState();
   }
 
@@ -79,59 +88,61 @@ class _CourseViewState extends State<CourseView> {
                   }),
             ),
             ElevatedButton(
-                onPressed: () {
-                  // if (isClicked.any((element) => element == true)) {
-                  //   final result = await insertStudent(
-                  //       widget.fnamecontroller,
-                  //       widget.lnamecontroller,
-                  //       widget.dateController,
-                  //       widget.addresscontroller,
-                  //       widget.religioncontroller,
-                  //       widget.nationalitycontroller,
-                  //       widget.gender,
-                  //       selectedCourses);
-                  //   if (result.statusCode == 200) {
-                  //     final responseData = json.decode(result.body);
-                  //     if (responseData['status'] == 'success') {
-                  //       print('Data inserted successfully');
-                  //       AwesomeDialog(
-                  //           context: context,
-                  //           title: 'Student Is Inserted Successfully',
-                  //           body: Center(
-                  //               child: Column(
-                  //                 children: [
-                  //                   const Divider(
-                  //                     color: Colors.grey,
-                  //                     thickness: 2,
-                  //                   ),
-                  //                   const SizedBox(height: 15),
-                  //                   Text('Student Is Inserted Successfully'),
-                  //                   const SizedBox(height: 10),
-                  //                   ElevatedButton(
-                  //                       onPressed: () {
-                  //                         Navigator.pop(context);
-                  //                         Navigator.pop(context);
-                  //                         Navigator.pop(context);
-                  //                       },
-                  //                       style: ElevatedButton.styleFrom(
-                  //                         shape: RoundedRectangleBorder(
-                  //                           borderRadius: BorderRadius.circular(30),
-                  //                         ),
-                  //                       ),
-                  //                       child: const Text('Close!')),
-                  //                 ],
-                  //               ))).show();
-                  //     } else {
-                  //       ShowAlterDialogMessage(context,
-                  //           'Error ${result.statusCode} ${result.reasonPhrase}');
-                  //     }
-                  //   }
-                  // } else {
-                  //   AwesomeDialog(
-                  //       context: context,
-                  //       title: 'please choose any courses')
-                  //       .show();
-                  // }
+                onPressed: () async{
+                  if (isClicked.any((element) => element == true)) {
+                    
+                    final result = await updateStudent(
+                        widget.curstd['Id'],
+                        widget.curstd['First_Name'],
+                        widget.curstd['Last_Name'],
+                        widget.curstd['Date_Of_Birth'],
+                        widget.curstd['Address'],
+                        widget.curstd['Religion'],
+                        widget.curstd['Nationality'],
+                        widget.curstd['Gender'],
+                        selectedCourses
+                      );
+                    if (result.statusCode == 200) {
+                      final responseData = json.decode(result.body);
+                      if (responseData['status'] == 'success') {
+                        AwesomeDialog(
+                            context: context,
+                            title: 'Student Is Inserted Successfully',
+                            body: Center(
+                                child: Column(
+                                  children: [
+                                    const Divider(
+                                      color: Colors.grey,
+                                      thickness: 2,
+                                    ),
+                                    const SizedBox(height: 15),
+                                    Text('Student Is Inserted Successfully'),
+                                    const SizedBox(height: 10),
+                                    ElevatedButton(
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                          Navigator.pop(context);
+                                          Navigator.pop(context);
+                                        },
+                                        style: ElevatedButton.styleFrom(
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.circular(30),
+                                          ),
+                                        ),
+                                        child: const Text('Close!')),
+                                  ],
+                                ))).show();
+                      } else {
+                        ShowAlterDialogMessage(context,
+                            'Error ${result.statusCode} ${result.reasonPhrase}');
+                      }
+                    }
+                  } else {
+                    AwesomeDialog(
+                        context: context,
+                        title: 'please choose any courses')
+                        .show();
+                  }
                 },
                 child: const Text('Enroll'))
           ],
